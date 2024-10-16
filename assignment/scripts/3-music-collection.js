306e85d2d66f2973a15491a6e99d574db90d3147
Junior Avalos <junior.trey.avalos13@gmail.com>
2024-10-16 13:07:24 -0500

    added the skeleton of the function with no code in it

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -2,6 +2,9 @@ console.log('***** Music Collection *****')
 // Safe Zone -- Write code below this line
 let myCollection = [];
 
+function addToCollection( collection, title, artist, yearPublished ) {
+
+}
 
 
 
